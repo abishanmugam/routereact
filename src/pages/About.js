c989b98c4928af43648241ce19ac0ad05c6d31ec
Mukesh Selvaraj <abirami@ishamistygreen.com>
2022-11-18 09:14:53 +0530

    comp

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div style={{ fontSize: '3rem', textAlign: 'center', margin: '1%' }}>
+      About
+    </div>
+  );
+};
+
+export default About;
